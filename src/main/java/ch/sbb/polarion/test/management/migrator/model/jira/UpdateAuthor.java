5af35e24d943c60e9d5d100e8d6a557a8584390b
Pavel Bezliapovich <pavel.bezliapovich@sbb.ch>
2025-04-01 16:37:59 +0200

    chore: fixed sonar issues

--- src/main/java/ch/sbb/polarion/test/management/migrator/model/jira/UpdateAuthor.java
+++ src/main/java/ch/sbb/polarion/test/management/migrator/model/jira/UpdateAuthor.java
@@ -1,15 +1,9 @@
 package ch.sbb.polarion.test.management.migrator.model.jira;
 
 import ch.sbb.polarion.test.management.migrator.model.CommonProperties;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
@@ -40,17 +34,4 @@ public class UpdateAuthor extends CommonProperties {
     public Boolean active;
     @JsonProperty("timeZone")
     public String timeZone;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new HashMap<>();
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
 }
--- src/main/java/ch/sbb/polarion/test/management/migrator/model/polarion/Author.java
+++ src/main/java/ch/sbb/polarion/test/management/migrator/model/polarion/Author.java
@@ -1,15 +1,9 @@
 package ch.sbb.polarion.test.management.migrator.model.polarion;
 
 import ch.sbb.polarion.test.management.migrator.model.CommonProperties;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
@@ -20,17 +14,4 @@ public class Author extends CommonProperties {
 
     @JsonProperty("data")
     private Data data;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new HashMap<>();
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
-
 }

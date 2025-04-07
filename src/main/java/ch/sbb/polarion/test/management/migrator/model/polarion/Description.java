42917759cb3019982e5f32a4121612057be1aef2
Pavel Bezliapovich <pavel.bezliapovich@sbb.ch>
2025-04-07 16:59:48 +0200

    chore: fixed sonar issues

--- src/main/java/ch/sbb/polarion/test/management/migrator/model/polarion/Description.java
+++ src/main/java/ch/sbb/polarion/test/management/migrator/model/polarion/Description.java
@@ -4,7 +4,6 @@ import ch.sbb.polarion.test.management.migrator.model.CommonProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 

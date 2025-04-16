9dce998bf1db3cfbd208cfed45579bd86c7c038d
Pavel Bezliapovich <pavel.bezliapovich@sbb.ch>
2025-04-16 08:56:48 +0200

    feat: added support for configuring custom fields
    Refs: #9

--- src/main/java/ch/sbb/polarion/test/management/migrator/model/polarion/WorkItems.java
+++ src/main/java/ch/sbb/polarion/test/management/migrator/model/polarion/WorkItems.java
@@ -7,14 +7,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
